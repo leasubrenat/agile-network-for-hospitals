c91a536bf8f4503d870ff967b36fa0d56a8b3bec
leasubrenat <leasubrenat@10.112.212.30>
2016-02-09 11:41:33 +0200

    board listing ok, but links need modification

--- src/java/com/lop/ApplicationConfig.java
+++ src/java/com/lop/ApplicationConfig.java
@@ -32,9 +32,11 @@ public class ApplicationConfig extends Application {
         resources.add(com.lop.CatResource.class);
         resources.add(com.lop.CatsResource.class);
         resources.add(com.lop.DogsResource.class);
+        resources.add(com.lop.api.BoardResource.class);
         resources.add(com.lop.api.BoardsResource.class);
         resources.add(com.lop.api.LocationsResource.class);
         resources.add(com.lop.api.PatientsResource.class);
+        resources.add(com.lop.api.RoleResource.class);
         resources.add(com.lop.api.RolesResource.class);
         resources.add(com.lop.api.UsersResource.class);
     }
