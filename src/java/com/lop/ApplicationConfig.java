65f5a95f7c6666cc05facf94a3c5259aa9e96802
Anh Do <trand@metropolia.fi>
2016-02-11 11:54:25 +0200

    Notification minor

--- src/java/com/lop/ApplicationConfig.java
+++ src/java/com/lop/ApplicationConfig.java
@@ -31,13 +31,17 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(com.lop.api.BoardResource.class);
         resources.add(com.lop.api.BoardsResource.class);
+        resources.add(com.lop.api.LocationResource.class);
         resources.add(com.lop.api.LocationsResource.class);
         resources.add(com.lop.api.NotificationResource.class);
         resources.add(com.lop.api.NotificationsResource.class);
+        resources.add(com.lop.api.PatientResource.class);
         resources.add(com.lop.api.PatientsResource.class);
         resources.add(com.lop.api.PostResource.class);
         resources.add(com.lop.api.PostsResource.class);
+        resources.add(com.lop.api.RoleResource.class);
         resources.add(com.lop.api.RolesResource.class);
+        resources.add(com.lop.api.TaskResource.class);
         resources.add(com.lop.api.TasksResource.class);
         resources.add(com.lop.api.UserResource.class);
         resources.add(com.lop.api.UsersResource.class);
